7d28f6b8a2393ce81e0e91630fedfc1dced7b123
Durgesh Singh <singh.durgesh2011@gmail.com>
2022-07-18 00:32:57 +0530

    Style and some functional implementation

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -13,41 +13,64 @@ export class MyComponent {
   
   isCaseSentive: boolean = false;
   delimiter: string = ' ';
-  words: string[];
+  words: string[] = [];
+  delimiters = [{ name: 'Select', value: ''}, { name: 'space', value: ' '}, { name: 'comma', value: ','}];
 
   private onInputChangeHandler = (event: Event) => {
     const {name, value} = (event.target as HTMLInputElement);
     this[name] = value;
-
-    // if(this.documentText){
-    //   this.maskedText = this.replaceAll(this.documentText, this.maskedText, 'XXXX')
-    // }
   }
 
   private onMaskedClick = () => {
-    this.words = this.phases.split(this.delimiter);
+    // this.words = this.phases.split(this.delimiter);
+    let temp = '', isPhasesStarted = false;
+    for(let i = 0; i < this.phases.length; i++){
+      if(this.phases[i] === this.delimiter && !isPhasesStarted){
+        this.words?.push(temp);
+        temp = '';
+      } else if([`'`, `"`].includes(this.phases[i])) {
+        isPhasesStarted = !isPhasesStarted;
+      } else {
+        temp += this.phases[i];
+      }
+    }
+    if(temp) this.words.push(temp);
+
     this.maskedText = this.documentText;
+    console.log('this.words: ', this.words);
+    
     this.words.forEach((word) => {
       this.maskedText = replaceAll(this.maskedText, word, generateMaskString(word?.length), { isCaseSentive: this.isCaseSentive })
     })
   }
 
+  private onDelimiterSelection = (event: Event) => {
+    console.log('Delimiter: ', (event.target as HTMLSelectElement).value);
+    
+    this.delimiter = (event.target as HTMLSelectElement).value;
+  }
+
   render() {
     return <div class="mctcontainer">
         <div class="mctcontainer__header">
-          <h3>Mask Censored Text</h3>
+          <h1>Mask Censored Text</h1>
         </div>
         <div class="mctcontainer__main">
           <div class="mctcontainer__main__input_phases">
-            <label>Keywords and phrases</label>
-            <input class="input__phases" name="phases" value={this.phases} onInput={this.onInputChangeHandler}/>
+            <h3>Keywords and phrases: </h3>
+            <div class="input">
+              <input class="input__phases" name="phases" value={this.phases} onInput={this.onInputChangeHandler}/>
+              <select class="input__delimiter" onChange={this.onDelimiterSelection}>
+                {this.delimiters.map(item => <option value={item.value}>{item.name}</option>)}
+              </select>
+            </div>
           </div>
-          <div>
+          <div class="mctcontainer__main__action">
             <button onClick={this.onMaskedClick}>Masked</button>
           </div>
-          <div class="mctcontainer__main__input_text">
-            <textarea class="textarea" name="documentText" value={this.documentText} onInput={this.onInputChangeHandler}/>
-            <label class="textarea label">{this.maskedText}</label>
+          <div class="mctcontainer__main__panel">
+            <textarea class="mctcontainer__main__panel--input-area textarea" name="documentText" value={this.documentText} onInput={this.onInputChangeHandler}/>
+            <p class="mctcontainer__main__panel--masked-text label">{this.maskedText}</p>
           </div>
         </div>
     </div>;
